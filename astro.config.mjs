1391042b90efddf8988961810c62af601a4a612f
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-09-07 11:29:25 -0500

    remove tailwind, hero banner, footer, placeholder text

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bornyesterdaybakery.com',
-  integrations: [tailwind()],
+  scopedStyleStrategy: 'class',
+  build: {
+    inlineStylesheets: 'always',
+  },
 })
